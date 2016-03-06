b2a044fad6a89583ad5baeea00c134e7fc488d62
Patricia <pa1809ty@gmail.com>
2016-03-06 09:49:33 +0000

    Find practitioners within a distance

--- app/controllers/practitioners_controller.rb
+++ app/controllers/practitioners_controller.rb
@@ -1,11 +1,17 @@
 class PractitionersController < ApplicationController
   def index
     postcode = params[:postcode]
+
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{postcode}+UK&key=#{Rails.application.secrets.key_google_maps}"
+    response = HTTParty.get(url, verify: false)
+    coordinates = response["results"][0]["geometry"]["location"]
+    distance = 5
+
     binding.pry
-    lat_lng = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{postcode}&key=#{Rails.application.secrets.key_google_maps}")
-    
+    @practitioners = Practitioner.near([coordinates["lat"].to_f, coordinates["lng"].to_f], distance)
+
     respond_to do |format|
-      format.json { render :json => postcode.to_json }
+      format.json { render :json => coordinates.to_json }
     end
   end
 end
