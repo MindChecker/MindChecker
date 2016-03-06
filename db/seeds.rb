dbb6d0a3e29825a67faa5c3f84bdb6103c2a227a
Patricia <pa1809ty@gmail.com>
2016-03-06 07:49:53 +0000

    Remove binding.pry from seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ require 'json'
 require 'pry'
 
 JSON.parse(open("/Users/claudiocarmeli/projects/NHS_app/lib/tasks/gps.json").read).each do |gp|
-  
+
   gp_as_json = {}
   gp_as_json["name"] = gp["OrganisationName"]
   address = "#{gp["Address1"]}, #{gp["Address2"]}, #{gp["Address3"]}, #{gp["City"]}, #{gp["County"]}, #{gp["Postcode"]}"
@@ -10,7 +10,7 @@ JSON.parse(open("/Users/claudiocarmeli/projects/NHS_app/lib/tasks/gps.json").rea
   gp_as_json["phone"] = gp["Phone"]
   gp_as_json["email"] = gp["Email"]
   gp_as_json["website"] = gp["Website"]
-  binding.pry
+
   practitioner = Practitioner.new(gp_as_json)
   practitioner.save
 end
