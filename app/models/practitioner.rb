8006651b4f030bbe70a16c868bed5f60239aa966
Patricia <pa1809ty@gmail.com>
2016-03-06 07:47:50 +0000

    Remove binding pry

--- app/models/practitioner.rb
+++ app/models/practitioner.rb
@@ -1,4 +1,4 @@
 class Practitioner < ActiveRecord::Base
   geocoded_by :address
-  before_save :geocode, if: ->(obj){ binding.pry;obj.address.present? and obj.address_changed? }
+  before_save :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
 end
