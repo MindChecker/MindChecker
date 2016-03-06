86b9e28884fff4105cb31824798adedeb7dfd26d
Claudio Carmeli <claudiocarmeli@yahoo.com>
2016-03-06 07:07:52 +0000

    Add index action to list practitioners

--- app/controllers/practitioners_controller.rb
+++ app/controllers/practitioners_controller.rb
@@ -1,4 +1,11 @@
 class PractitionersController < ApplicationController
   def index
+    postcode = params[:postcode]
+    binding.pry
+    lat_lng = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{postcode}&key=#{Rails.application.secrets.key_google_maps}")
+    
+    respond_to do |format|
+      format.json { render :json => postcode.to_json }
+    end
   end
 end
